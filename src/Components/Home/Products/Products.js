7eafade6b629f96bba192219d3da777baeb59fbe
joy <juniorbat0003@gmail.com>
2022-11-24 21:12:08 +0600

    Reeact photoview added

--- src/Components/Home/Products/Products.js
+++ src/Components/Home/Products/Products.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PrimaryButton from '../../PrimaryButton/PrimaryButton';
+import kitchen1 from '../../../assets/kitchen1.avif';
+import Gallery from '../../Gallery/Gallery';
 
 const Products = () => {
     return (
         <div className='my-24'>
             <div className='lg:flex md:flex justify-center gap-5'>
                 <div className="card card-compact my-5 w-96 bg-base-100 shadow-xl">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <figure> <Gallery
+                        img={kitchen1}
+                    ></Gallery> </figure>
                     <div className="card-body">
                         <h2 className="card-title">Shoes!</h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
